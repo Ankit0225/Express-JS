29a95d84b49a90c2d0b5591c09ab2f8c6f6f9e5e
Ankit <ankitmaurya0225@gmail.com>
2021-05-09 21:08:32 +0530

    express-first-app

--- express-first-app/server.js
+++ express-first-app/server.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/hello',(req,res) => {
-    res.send('Hello World')
+    let name = 'guest'
+    if(req.query.user) { name = req.query.user }
+    res.send('Hello World ' + name)
 })
 app.use('/abc',express.static(__dirname + '/public'))
 
